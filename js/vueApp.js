819eb1072d2522e32c3ed8979dc0fc2a6ceb201e
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-05-28 20:08:37 -0300

    last changes

--- js/vueApp.js
+++ js/vueApp.js
@@ -1,3 +1,18 @@
+let myOjbJSON = {"userId":1,"id":2,"title":"qui est esse","body":"est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"}
+
+let myObj = {
+    userId: 1,
+    id: 2,
+    title: "qui est esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  }
+
+console.log(myObj);
+console.log(JSON.stringify(myObj)); // convierto a JSON
+
+console.log(myOjbJSON);
+//console.log(JSON.parse(myOjbJSON)); // convierto a objeto
+
 let myComponent = new Vue(
     {
     el: '#app',
